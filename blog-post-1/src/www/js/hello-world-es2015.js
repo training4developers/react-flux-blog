35a0b1ebc6f3e524aeda2376158e9f5b764e4d59
Eric W. Greene <eric@ericwgreene.com>
2016-01-14 06:52:36 -0500

    added more demos

---
+++ blog-post-1/src/www/js/hello-world-es2015.js
@@ -0,0 +1,17 @@
+(function() {
+
+	"use strict";
+
+	var HelloWorld = React.createClass({
+
+		render: function() {
+			return React.createElement("h1", null, "Hello World!");
+		},
+
+	});
+
+	var mainElement = document.querySelector("main");
+
+	ReactDOM.render(React.createElement(HelloWorld), mainElement);
+	
+})();
--- blog-post-1/src/www/js/index.jsx
+++ blog-post-1/src/www/js/index.jsx
@@ -3,7 +3,51 @@
 	"use strict";
 
 	let demos = [
-		{ name: "Hello World!", description: "Hello World Desc!", url: "hello-world.html" }
+		{
+			name: "React.js Hello World Demo",
+			description: "A simple Hello World demonstration of React.js.",
+			url: "hello-world.html"
+		},
+		{
+			name: "React.js Hello World JSX Demo",
+			description: "A simple Hello World JSX demonstration of React.js.",
+			url: "hello-world-jsx.html"
+		},
+		{
+			name: "React.js Hello World ES2015 Demo",
+			description: "Demonstrates how to use ES2015 class syntax to create a new component by extending the base React.Component.",
+			url: "hello-world-es2015.html"
+		},
+		{
+			name: "React.js JS Properties Demo",
+			description: "Demonstrates how to use createElement to pass a properties object to a React Component.",
+			url: "props.html"
+		},
+		{
+			name: "React.js JSX Properties Demo",
+			description: "Demonstrates how to pass a static property value to a React Component using JSX.",
+			url: "jsx-props.html"
+		},
+		{
+			name: "React.js JSX Variable Property Demo",
+			description: "Demonstrates how to use JSX to pass a property to a component. The property is set using a JavaScript variable.",
+			url: "jsx-varprops.html"
+		},
+		{
+			name: "React.js Child Content Demo",
+			description: "Demonstrates how to add a single child content element to a component's element.",
+			url: "child-content.html"
+		},
+		{
+			name: "React.js Child Content List Demo",
+			description: "Displays an unordered list of child content list items.  The items are added as an array of items as the child content of the component element.",
+			url: "child-content-list.html"
+		},
+		{
+			name: "React.js Composable Components Demo",
+			description: "Demonstrates how to build a composable component through the use of multiple React Components.",
+			url: "composable-components.html"
+		}
 	];
 
 	class DemoTable extends React.Component {
@@ -15,7 +59,7 @@
 			});
 			
 			return (
-				<table>
+				<table className='table table-striped'>
 					<thead>
 						<tr>
 							<th>Name</th>
